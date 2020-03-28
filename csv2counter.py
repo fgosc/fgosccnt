9a149a08d16ff12bc9811c254b7be17d8819a733
max747 <takanori.kojima@gmail.com>
2020-03-28 10:26:14 +0000

    変数名の表記ゆれを揃える。

--- csv2counter.py
+++ csv2counter.py
@@ -7,7 +7,7 @@ import argparse
 monyupi_list = ['剣モ', '弓モ', '槍モ', '騎モ', '術モ', '殺モ', '狂モ',
                 '剣ピ', '弓ピ', '槍ピ', '騎ピ', '術ピ', '殺ピ', '狂ピ', ]
 
-skilstone_list = [  '剣秘', '弓秘', '槍秘', '騎秘', '術秘', '殺秘', '狂秘',
+skillstone_list = [ '剣秘', '弓秘', '槍秘', '騎秘', '術秘', '殺秘', '狂秘',
                     '剣魔', '弓魔', '槍魔', '騎魔', '術魔', '殺魔', '狂魔',
                     '剣輝', '弓輝', '槍輝', '騎輝', '術輝', '殺輝', '狂輝']
 
@@ -37,9 +37,9 @@ if __name__ == '__main__':
         if i == 2:
             print (l[0][item] + "周")
         if i > 2:
-            if skillstone_flag == False and item in skilstone_list:
+            if skillstone_flag == False and item in skillstone_list:
                 output = output[:-1] + "\n"
-                skillstone_flag = True                
+                skillstone_flag = True
             elif skillstone_flag == True and item not in skillstone_list:
                 output = output[:-1] + "\n"
                 skillstone_flag = False
@@ -68,4 +68,3 @@ if __name__ == '__main__':
 
     print (output[:-1])
     print ("#FGO周回カウンタ http://aoshirobo.net/fatego/rc/")
-
