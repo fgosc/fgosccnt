6e670463cd90c831b55bb4131e071a9cf7a6140f
fgophi <fgophi@gmail.com>
2021-08-24 06:38:18 +0900

    fix for magic gem of sword

--- fgosccnt.py
+++ fgosccnt.py
@@ -2192,8 +2192,8 @@ class Item:
         """
         height, width = img_rgb.shape[:2]
 
-        img = img_rgb[int((145-16-60*0.8)/2/145*height)+3:
-                      int((145-16+60*0.8)/2/145*height)+3,
+        img = img_rgb[int((145-16-60*0.8)/2/145*height)+2:
+                      int((145-16+60*0.8)/2/145*height)+2,
                       int((132-52*0.8)/2/132*width):
                       int((132+52*0.8)/2/132*width)]
 
