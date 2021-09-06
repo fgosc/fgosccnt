e68e07feae31bbaf9ce2a29729d9111ec8629ca5
fgophi <fgophi@gmail.com>
2021-09-07 01:35:43 +0900

    ワイドスクリーンの読み取り位置を正確な位置に変更

--- fgosccnt.py
+++ fgosccnt.py
@@ -501,18 +501,12 @@ class ScreenShot:
         # 相対座標(新UI)
         rx = self.find_notch()
         height, width = img.shape[:2]
-        # dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 17,
-        #                 width - 430: width - 370]
         if width/height > 16/8.99:
-            dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 15,
-                            width - 490 - rx: width - 430 - int(rx*resize_scale)]
-        #     dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 17,
-        #                    int(self.x1*resize_scale) + 1400: int(self.x1*resize_scale) + 1705]
+            dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 14,
+                            width - 495 - rx: width - 435 - int(rx*resize_scale)]
         else:
-            dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 15,
+            dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 14,
                             width - 430 - rx: width - 370 - int(rx*resize_scale)]
-        #     dcnt_new = img[int(self.y1*resize_scale) - 20: int(self.y1*resize_scale) + 17,
-        #                    int(self.x1*resize_scale) + 1463: int(self.x1*resize_scale) + 1530]
         if display:
             cv2.imshow('image', dcnt_new)
             cv2.waitKey(0)
@@ -887,9 +881,6 @@ class ScreenShot:
             newimg_th[height - 1, w] = 0
             newimg_th[height - 2, w] = 0
             newimg_th[height - 3, w] = 0
-        # cv2.imshow('image',newimg_th)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
         res = self.pred_dcnt(newimg_th)
         return res
